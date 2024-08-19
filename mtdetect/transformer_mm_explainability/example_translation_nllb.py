47519cf3ced76fb89ecff7ecff73a5b8e13de26c
Cristian García Romero <cgr71ii@gmail.com>
2024-08-19 21:28:51 +0200

    Explainability rules WIP

--- mtdetect/transformer_mm_explainability/example_translation_nllb.py
+++ mtdetect/transformer_mm_explainability/example_translation_nllb.py
@@ -3,6 +3,7 @@ import sys
 
 import torch
 import transformers
+import numpy as np
 
 # NLLB supported languages: https://github.com/facebookresearch/flores/blob/main/flores200/README.md#languages-in-flores-200
 
@@ -120,7 +121,7 @@ for i in range(max_length):
         # Next token might be different of EoS -> force
         if next_token_id != tokenizer.eos_token_id:
             if debug:
-                print(f"DEBUG: last generated token is not EoS but {tokenizer.convert_ids_to_tokens(next_token_id)} (id: {next_token_id})")
+                print(f"DEBUG: last generated token is not EoS but {tokenizer.convert_ids_to_tokens(next_token_id)} (id: {next_token_id}). This is expected due to teacher forcing")
 
             next_token_id = tokenizer.eos_token_id
 
@@ -144,36 +145,34 @@ def capture_attention_grads(layer, component):
 
     return _f
 
-encoder_attentions = {l: a for l, a in enumerate(model_output.encoder_attentions)}
-decoder_attentions = {l: a for l, a in enumerate(model_output.decoder_attentions)}
-cross_attentions = {l: a for l, a in enumerate(model_output.cross_attentions)}
+assert len(model_output.encoder_attentions) == num_hidden_layers, f"{len(model_output.encoder_attentions)} != {num_hidden_layers}"
+assert len(model_output.decoder_attentions) == num_hidden_layers, f"{len(model_output.decoder_attentions)} != {num_hidden_layers}"
+assert len(model_output.cross_attentions) == num_hidden_layers, f"{len(model_output.cross_attentions)} != {num_hidden_layers}"
+
+attentions = {l: {
+    "encoder": model_output.encoder_attentions[l],
+    "decoder": model_output.decoder_attentions[l],
+    "cross": model_output.cross_attentions[l],
+    } for l in range(num_hidden_layers)}
 
 for layer in range(num_hidden_layers):
-    assert encoder_attentions[layer].requires_grad
-    assert decoder_attentions[layer].requires_grad
-    assert cross_attentions[layer].requires_grad
+    for component in ("encoder", "decoder", "cross"):
+        attentions[layer][component].requires_grad
 
-    encoder_attentions[layer].retain_grad()
-    decoder_attentions[layer].retain_grad()
-    cross_attentions[layer].retain_grad()
-    encoder_attentions[layer].register_hook(capture_attention_grads(layer, "encoder"))
-    decoder_attentions[layer].register_hook(capture_attention_grads(layer, "decoder"))
-    cross_attentions[layer].register_hook(capture_attention_grads(layer, "cross"))
-
-assert len(encoder_attentions) == num_hidden_layers, f"{len(encoder_attentions)} != {num_hidden_layers}"
-assert len(decoder_attentions) == num_hidden_layers, f"{len(decoder_attentions)} != {num_hidden_layers}"
-assert len(cross_attentions) == num_hidden_layers, f"{len(cross_attentions)} != {num_hidden_layers}"
+        attentions[layer][component].retain_grad()
+        attentions[layer][component].register_hook(capture_attention_grads(layer, component))
 
 source_text_seq_len = len(inputs.input_ids[0])
 target_text_seq_len = len(generated_tokens)
-encoder_attention_expected_shape = (batch_size, num_attention_heads, source_text_seq_len, source_text_seq_len)
-decoder_attention_expected_shape = (batch_size, num_attention_heads, target_text_seq_len, target_text_seq_len)
-cross_attention_expected_shape = (batch_size, num_attention_heads, target_text_seq_len, source_text_seq_len)
+attention_expected_shape = {
+    "encoder": (batch_size, num_attention_heads, source_text_seq_len, source_text_seq_len),
+    "decoder": (batch_size, num_attention_heads, target_text_seq_len, target_text_seq_len),
+    "cross": (batch_size, num_attention_heads, target_text_seq_len, source_text_seq_len),
+    }
 
 for l in range(num_hidden_layers):
-    assert encoder_attentions[l].shape == encoder_attention_expected_shape, f"{l}: {encoder_attentions[l].shape} != {encoder_attention_expected_shape}"
-    assert decoder_attentions[l].shape == decoder_attention_expected_shape, f"{l}: {decoder_attentions[l].shape} != {decoder_attention_expected_shape}"
-    assert cross_attentions[l].shape == cross_attention_expected_shape, f"{l}: {cross_attentions[l].shape} != {cross_attention_expected_shape}"
+    for component in ("encoder", "decoder", "cross"):
+        assert attentions[l][component].shape == attention_expected_shape[component], f"{l}: {attentions[l][component].shape} != {attention_expected_shape[component]}"
 
 translated_tokens = torch.tensor([generated_tokens]).to(device)
 
@@ -206,21 +205,79 @@ output = decode(translated_tokens)
 for translated_text in output:
     print(translated_text)
 
-if debug and not teacher_forcing:
+if debug:
     from_generate = translate_from_generate()
-
-    print(f"DEBUG: from generate(): {from_generate}")
 
     assert len(from_generate) == len(output)
 
     for o1, o2 in zip(output, from_generate):
-        assert o1 == o2, f"{o1} vs {o2}"
+        if o1 != o2:
+            print(f"DEBUG: warning: different result than using generate (this is expected if teacher forcing is enabled): {o1} vs {o2}")
 
     from_pipeline = translate_from_pipeline()
-
-    print(f"DEBUG: from pipeline(): {from_pipeline}")
 
     assert len(from_pipeline) == len(output)
 
     for o1, o2 in zip(output, from_pipeline):
-        assert o1 == o2, f"{o1} vs {o2}"
+        if o1 != o2:
+            print(f"DEBUG: warning: different result than using pipeline (this is expected if teacher forcing is enabled): {o1} vs {o2}")
+
+# Apply explainability
+# i -> source, t -> target (following paper nomenclature)
+
+r_ii = np.identity(source_text_seq_len)
+r_tt = np.identity(target_text_seq_len)
+r_it = np.zeros((source_text_seq_len, target_text_seq_len)) # influence of the input text on the translated text
+#r_ti = np.zeros((target_text_seq_len, source_text_seq_len)) # We only have co-attention in the decoder (i.e., r_it)
+a_line = {}
+
+for layer in range(num_hidden_layers):
+    a_line[layer] = {}
+
+    for component in ("encoder", "decoder", "cross"):
+        assert component not in a_line[layer]
+
+        gradient = attentions_grad_store[layer][component]
+        attention = attentions[layer][component]
+
+        assert gradient.shape == attention.shape, f"{gradient.shape} != {attention.shape}"
+
+        a_line_aux = gradient * attention
+
+        assert a_line_aux.shape == attention.shape, f"{a_line_aux.shape} != {attention.shape}"
+        assert a_line_aux.shape == attention_expected_shape[component], f"{a_line_aux.shape} != {attention_expected_shape[component]}"
+
+        a_line_aux = torch.max(a_line_aux, torch.zeros_like(a_line_aux)).cpu().detach()
+        a_line_aux = torch.mean(a_line_aux, dim=1)
+
+        a_line[layer][component] = a_line_aux
+
+        assert a_line[layer][component].shape == (batch_size, *attention_expected_shape[component][-2:]), a_line[layer].shape
+
+assert len(a_line) == num_hidden_layers
+
+# Update attention layers
+
+for layer in range(num_hidden_layers):
+
+    # equation 6
+    for component, r in (("encoder", r_ii), ("decoder", r_tt)):
+        a_line_aux = a_line[layer][component][0].numpy() # current layer, and batch size is 0
+
+        assert a_line_aux.shape == attention_expected_shape[component][-2:], a_line_aux.shape
+        assert a_line_aux.shape == r.shape
+
+        r += np.matmul(a_line_aux, r)
+
+    # equation 7
+    for component, r in (("encoder", r_it),): # is this equation useful for anything...??? (result is always 0...) (for cross attention we use the encoder A variable according
+                                              #  ... to https://github.com/hila-chefer/Transformer-MM-Explainability/blob/main/lxmert/lxmert/src/ExplanationGenerator.py#L169 )
+        a_line_aux = a_line[layer][component][0].numpy()
+
+        assert a_line_aux.shape == attention_expected_shape[component][-2:], a_line_aux.shape
+
+        r += np.matmul(a_line_aux, r)
+
+        assert (r == np.zeros_like(r)).all()
+
+    # TODO equation 8, 9, 10, and 11
