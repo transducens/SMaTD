dc951ed054b1add7f4e9c587fc1504c5c72b1e7a
Cristian García Romero <cgr71ii@gmail.com>
2025-02-05 17:17:12 +0100

    M2M translation script fix

--- mtdetect/translation/m2m.py
+++ mtdetect/translation/m2m.py
@@ -9,18 +9,18 @@ import transformers
 source_lang = sys.argv[1] # e.g., en (check https://huggingface.co/facebook/m2m100_1.2B)
 target_lang = sys.argv[2]
 batch_size = int(sys.argv[3]) if len(sys.argv) > 3 and len(sys.argv[3]) > 0 else 16
-pretrained_model = sys.argv[4] if len(sys.argv) > 4 else "facebook/m2m100_418M"
+pretrained_model = sys.argv[4] if len(sys.argv) > 4 else "facebook/m2m100-12B-last-ckpt"
+beam_size = int(sys.argv[5]) if len(sys.argv) > 5 and len(sys.argv[5]) > 0 else 5 # Paper: "Unless otherwise specified: [...], use beam search with beam 5"
 
 assert batch_size > 0, batch_size
+assert beam_size > 0, beam_size
 
 sys.stderr.write(f"Translating from {source_lang} to {target_lang} (pretrained_model={pretrained_model})\n")
 
 model, device = util.load_model(pretrained_model, transformers.AutoModelForSeq2SeqLM)
 tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model, src_lang=source_lang, tgt_lang=target_lang)
 target_lang_id = tokenizer.get_lang_id(target_lang)
-beam_size = model.generation_config.num_beams
-max_new_tokens = model.generation_config.max_length
-early_stopping = True if beam_size > 1 else False
+max_new_tokens = 1024 # not available: model.generation_config.max_length
 
 def translate(batch, device):
     _model = model
@@ -29,7 +29,7 @@ def translate(batch, device):
         _model = model.to(device)
 
     inputs = tokenizer(batch, return_tensors="pt", add_special_tokens=True, truncation=True, padding=True).to(device)
-    result = _model.generate(**inputs, forced_bos_token_id=target_lang_id, max_new_tokens=max_new_tokens, num_beams=beam_size, early_stopping=early_stopping)
+    result = _model.generate(**inputs, forced_bos_token_id=target_lang_id, max_new_tokens=max_new_tokens, num_beams=beam_size)
     output = tokenizer.batch_decode(result, skip_special_tokens=True)
 
     return output
